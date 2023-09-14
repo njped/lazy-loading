10587f4bd67e99a1764c6caacbd075f8ab03c6b8
Nathan Pedigo <njpedigo@gmail.com>
2023-09-14 07:42:33 -0600

    created lazy loading repo

---
+++ src/app/app.config.ts
@@ -0,0 +1,17 @@
+// import { ApplicationConfig } from '@angular/core';
+// import {
+//   PreloadAllModules,
+//   provideRouter,
+//   withPreloading,
+// } from '@angular/router';
+
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(
+//       routes,
+//       withPreloading(PreloadAllModules)
+//     ),
+//   ],
+// };
---
+++ src/app/crisis-detail-resolver.service.ts
@@ -0,0 +1,14 @@
+// import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+// import { Observable } from 'rxjs';
+
+// /* An interface that represents your data model */
+// export interface Crisis {
+//   id: number;
+//   name: string;
+// }
+
+// export class CrisisDetailResolverService implements Resolve<Crisis> {
+//   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Crisis> {
+//     // your logic goes here
+//   }
+// }
